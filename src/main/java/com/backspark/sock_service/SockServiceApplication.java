d4faf3573f4db2ae8dd88912dfe46693c2b9093b
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-12-20 02:42:46 +0300

    init

---
+++ src/main/java/com/backspark/sock_service/SockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backspark.sock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backspark/sock_service/SockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backspark.sock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
